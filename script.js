2857b0f46d41d3bc2c398b2b895c979e736aca14
Uday Jamgade <udayjamgade1@gmail.com>
2024-11-03 21:31:39 +0530

    Frontend Complete

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ const inputDate= document.getElementById("date").value;
 const inputtextarea= document.getElementById("textarea").value;
 const Button= document.getElementById("button");
 const container = document.getElementById("content");
+const show=document.getElementById("show");
 
 
-console.log(inputCategory);
+const toggle = false;
+show.addEventListener("click", function(){
+    const form=document.getElementById("section1");
+    if (form.style.display === 'none' || form.style.display === '') { form.style.display = 'block'; } else { form.style.display = 'none'; }
+})
+
+
+
+
 
 
 inputCategory.onchange=function(){
@@ -33,34 +42,49 @@ const container = document.getElementById("content");
 
 
     const post= document.createElement("div");
-    post.classList.add="post-box ,tech";
+    post.classList.add("post-box");
+    // post.classList.add(`${inputCategory}`)
     const img = document.createElement("img");
     img.src = URL.createObjectURL(inputfile.files[0]);
-    img.classList.add= "post-img";
+    img.classList.add("post-img");
 
-    const category = document.createElement("h1");
-    category.classList.add= "category";
+    const category = document.createElement("h2");
+    category.classList.add("category");
     category.textContent=  inputCategory;
+    
+
+    
 
     const headline = document.createElement("h1");
     headline.href ="#"
-    headline.classList.add ="post-title";
+    headline.classList.add("post-title");
     headline.textContent= inputheadline;
 
     const date = document.createElement("p");
-    date.classList.add="post-date";
+    date.classList.add("post-date");
     date.innerText= inputDate;
 
 
     const description = document.createElement("p");
-    description.classList.add = "post-description";
+    description.classList.add("post-description");
     description.innerText = inputtextarea;
+
+    const button= document.createElement("div")
+    button.classList.add("profile")
+
+    const btn=document.createElement("button");
+    const a= document.createElement("a");
+    a.textContent="Read More";
 
 
     post.appendChild(img);
     post.appendChild(category);
     post.appendChild(headline);
+    post.appendChild(date);
     post.appendChild(description);
+    btn.appendChild(a);
+    button.appendChild(btn);
+    post.appendChild(button);
     container.appendChild(post);
 
     console.log(date.textContent);
@@ -70,9 +94,24 @@ const container = document.getElementById("content");
     document.getElementById("category").value= " ";
     document.getElementById("headline").value="";
     document.getElementById("textarea").value="";
-    document.getElementById("image").src="";
+    document.getElementById("image").src="https://image.freepik.com/free-vector/input_53876-25528.jpg";
+    document.getElementById("date").value="";
     document.getElementById("file");
+
+
+    const form=document.getElementById("section1");
+    form.style.display="none"
+
 })
+
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
+
+
 
 
 
